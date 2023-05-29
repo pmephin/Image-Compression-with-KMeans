421b421ee4e289412767b5bb722c52f2320a58cf
pmephin <134229875+pmephin@users.noreply.github.com>
2023-05-29 13:43:40 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 from compress_utils import *
 
-image = io.imread('data/test_image.jpeg)
+image = io.imread('data/test_image.jpeg')  # Select Image
+
+compressed_image=compress_eig(image,k1=128,k2=256)   
+#Use `compress_reg` for regular compression and `compress_var` for amplitude variance compression
+io.imsave('data/results/compressed_image.jpg',compressed_image)
+print(scores(compressed_image,image))
